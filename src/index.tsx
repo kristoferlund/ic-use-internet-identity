4e01c7bbd07401375fa3fe04c88d2d64afad6816
Kristofer <kristofer@fmckl.se>
2025-01-07 22:34:16 +0100

    Replacing React Context with xstate/store

--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,200 @@
-/* eslint-disable react-refresh/only-export-components */
-import { createContext, useContext, useEffect } from "react";
-import { type ReactNode, useState } from "react";
-import type { InternetIdentityContextType } from "./context.type";
+import { createStore } from "@xstate/store";
+import { useSelector } from "@xstate/store/react";
+import type { LoginStatus } from "./state.type";
+import { type ReactNode, useEffect } from "react";
 import {
   AuthClient,
   type AuthClientCreateOptions,
   type AuthClientLoginOptions,
 } from "@dfinity/auth-client";
-import type { State } from "./state.type";
 import type { LoginOptions } from "./login-options.type";
+import type { Identity } from "@dfinity/agent";
+import type { InternetIdentityContextType } from "./context.type";
+
+interface Context {
+  providerComponentPresent: boolean;
+  authClient?: AuthClient;
+  createOptions?: AuthClientCreateOptions;
+  loginOptions?: LoginOptions;
+  isInitializing: boolean;
+  loginStatus: LoginStatus;
+  loginError?: Error;
+  identity?: Identity;
+}
+
+const initialContext: Context = {
+  providerComponentPresent: false,
+  authClient: undefined,
+  createOptions: undefined,
+  loginOptions: undefined,
+  isInitializing: false,
+  loginStatus: "idle",
+  loginError: undefined,
+  identity: undefined,
+};
+
+const store = createStore({
+  context: initialContext,
+  on: {
+    setProviderComponentPresent: {
+      providerComponentPresent: (
+        _,
+        event: { providerComponentPresent: boolean },
+      ) => event.providerComponentPresent,
+    },
+    setAuthClient: {
+      authClient: (_, event: { authClient?: AuthClient }) => event.authClient,
+    },
+    setCreateOptions: {
+      createOptions: (_, event: { createOptions?: AuthClientCreateOptions }) =>
+        event.createOptions,
+    },
+    setLoginOptions: {
+      loginOptions: (_, event: { loginOptions?: LoginOptions }) =>
+        event.loginOptions,
+    },
+    setIsInitializing: {
+      isInitializing: (_, event: { isInitializing: boolean }) =>
+        event.isInitializing,
+    },
+    setLoginStatus: {
+      loginStatus: (_, event: { loginStatus: LoginStatus }) =>
+        event.loginStatus,
+    },
+    setIdentity: {
+      identity: (_, event: { identity?: Identity }) => event.identity,
+    },
+    setLoginError: {
+      loginError: (_, event: { loginError?: Error }) => event.loginError,
+    },
+  },
+});
 
 /**
- * React context for managing the login state and the Internet Identity.
+ * Create the auth client with default options or options provided by the user.
  */
-export const InternetIdentityContext = createContext<
-  InternetIdentityContextType | undefined
->(undefined);
+async function createAuthClient(): Promise<AuthClient> {
+  const createOptions = store.getSnapshot().context.createOptions;
+  const options: AuthClientCreateOptions = {
+    idleOptions: {
+      // Default behaviour of this hook is not to logout and reload window on indentity expiration
+      disableDefaultIdleCallback: true,
+      disableIdle: true,
+      ...createOptions?.idleOptions,
+    },
+    ...createOptions,
+  };
+  const authClient = await AuthClient.create(options);
+  store.send({ type: "setAuthClient", authClient });
+  return authClient;
+}
 
 /**
- * Hook to access the InternetIdentityContext.
+ * Connect to Internet Identity to login the user.
  */
-export const useInternetIdentity = (): InternetIdentityContextType => {
-  const context = useContext(InternetIdentityContext);
-  if (!context) {
-    throw new Error(
-      "useInternetIdentity must be used within an InternetIdentityProvider"
+async function login() {
+  const context = store.getSnapshot().context;
+
+  if (!context.providerComponentPresent) {
+    console.error(
+      "The InternetIdentityProvider component is not present. Make sure to wrap your app with it.",
     );
   }
-  return context;
+
+  let authClient = context.authClient;
+
+  if (!authClient) {
+    authClient = await createAuthClient();
+  }
+
+  if (await authClient.isAuthenticated()) {
+    throw new Error("User is already authenticated");
+  }
+
+  const loginOptions = context.loginOptions;
+  const options: AuthClientLoginOptions = {
+    identityProvider: process.env.II_URL,
+    onSuccess: onLoginSuccess,
+    onError: onLoginError,
+    maxTimeToLive: BigInt(3_600_000_000_000), // Defaults to 1 hour
+    ...loginOptions,
+  };
+
+  store.send({ type: "setLoginStatus", loginStatus: "logging-in" });
+  authClient.login(options);
+}
+
+/**
+ * Callback, login was successful. Saves identity to state.
+ */
+function onLoginSuccess() {
+  const identity = store.getSnapshot().context.authClient?.getIdentity();
+  if (!identity) {
+    throw new Error("Identity not found after successful login");
+  }
+  store.send({ type: "setIdentity", identity });
+  store.send({ type: "setLoginStatus", loginStatus: "success" });
+}
+
+/**
+ * Login was not successful. Sets loginError.
+ */
+function onLoginError(error?: string | undefined) {
+  store.send({ type: "setLoginStatus", loginStatus: "error" });
+  store.send({ type: "setLoginError", loginError: new Error(error) });
+}
+
+/**
+ * Clears the identity from the state and local storage. Effectively "logs the
+ * user out".
+ */
+async function clear() {
+  const authClient = store.getSnapshot().context.authClient;
+  if (!authClient) {
+    throw new Error("Auth client not initialized");
+  }
+  await authClient.logout();
+
+  store.send({ type: "setIdentity", identity: undefined });
+  store.send({ type: "setLoginStatus", loginStatus: "idle" });
+  store.send({ type: "setLoginError", loginError: undefined });
+  store.send({ type: "setAuthClient", authClient: undefined });
+}
+
+/**
+ * Hook to access the internet identity as well as login status along with
+ * login and clear functions.
+ */
+export const useInternetIdentity = (): InternetIdentityContextType => {
+  const context = useSelector(store, (state) => state.context);
+  return {
+    isInitializing: context.isInitializing,
+    login,
+    loginStatus: context.loginStatus,
+    loginError: context.loginError,
+    isLoggingIn: context.loginStatus === "logging-in",
+    isLoginError: context.loginStatus === "error",
+    isLoginSuccess: context.loginStatus === "success",
+    isLoginIdle: context.loginStatus === "idle",
+    clear,
+    identity: context.identity,
+  };
 };
 
 /**
- * Provider component for the InternetIdentityContext.
- * Manages identity state and provides authentication-related functionalities.
+ * The InternetIdentityProvider component makes the saved identity available
+ * after page reloads. It also allows you to configure default options
+ * for AuthClient and login.
  */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function InternetIdentityProvider({
+  children,
   createOptions,
   loginOptions,
-  children,
 }: {
+  /** The child components that the InternetIdentityProvider will wrap. This allows any child
+   * component to access the authentication context provided by the InternetIdentityProvider. */
+  children: ReactNode;
+
   /** Options for creating the {@link AuthClient}. See AuthClient documentation for list of options
    *
    *`ic-use-internet-identity` defaults to disabling the AuthClient idle handling (clearing identities
@@ -56,149 +211,35 @@ export function InternetIdentityProvider({
    */
   createOptions?: AuthClientCreateOptions;
 
-  /** Options that determine the behaviour of the {@link AuthClient} login call. These options are a subset of 
-   * the {@link AuthClientLoginOptions}. 
+  /** Options that determine the behaviour of the {@link AuthClient} login call. These options are a subset of
+   * the {@link AuthClientLoginOptions}.
    */
   loginOptions?: LoginOptions;
-
-  /** The child components that the InternetIdentityProvider will wrap. This allows any child
-   * component to access the authentication context provided by the InternetIdentityProvider. */
-  children: ReactNode;
 }) {
-  const [state, setState] = useState<State>({
-    isInitializing: true,
-    loginStatus: "idle",
-  });
-
   // Effect runs on mount. Creates an AuthClient and attempts to load a saved identity.
   useEffect(() => {
     (async () => {
-      // Add some default options to the auth client
-      const options: AuthClientCreateOptions = {
-        idleOptions: {
-          // Default behaviour of this hook is not to logout and reload window on indentity expiration
-          disableDefaultIdleCallback: true,
-          disableIdle: true,
-          ...createOptions?.idleOptions,
-        },
-        ...createOptions,
-      };
-
-      const authClient = await AuthClient.create(options);
+      store.send({
+        type: "setProviderComponentPresent",
+        providerComponentPresent: true,
+      });
+      store.send({ type: "setIsInitializing", isInitializing: true });
+      store.send({ type: "setCreateOptions", createOptions });
+      store.send({ type: "setLoginOptions", loginOptions });
+      let authClient = store.getSnapshot().context.authClient;
+      if (!authClient) {
+        authClient = await createAuthClient();
+      }
       const isAuthenticated = await authClient.isAuthenticated();
-
-      setState((prevState) => ({
-        ...prevState,
-        authClient,
-      }));
-
       if (isAuthenticated) {
         const identity = authClient.getIdentity();
         if (identity) {
-          setState((prevState) => ({
-            ...prevState,
-            identity,
-          }));
+          store.send({ type: "setIdentity", identity });
         }
       }
-
-      setState((prevState) => ({
-        ...prevState,
-        isInitializing: false,
-      }));
+      store.send({ type: "setIsInitializing", isInitializing: false });
     })();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [createOptions]); // State is not included in the dependency array
+  }, [createOptions, loginOptions]);
 
-  /**
-   * Callback, login was successful. Saves identity to state.
-   */
-  function onLoginSuccess() {
-    const identity = state.authClient?.getIdentity();
-    if (!identity) {
-      throw new Error("Identity not found after successful login");
-    }
-    setState((prevState) => ({
-      ...prevState,
-      loginStatus: "success",
-      identity,
-    }));
-  }
-
-  /**
-   * Login was not successful. Sets loginError.
-   */
-  function onLoginError(error?: string | undefined) {
-    setState((prevState) => ({
-      ...prevState,
-      loginStatus: "error",
-      loginError: new Error(error),
-    }));
-  }
-
-  /**
-   * Connect to Internet Identity to login the user.
-   */
-  async function login() {
-    if (!state.authClient) {
-      throw new Error("Auth client not initialized");
-    }
-
-    if (await state.authClient.isAuthenticated()) {
-      throw new Error("User is already authenticated");
-    }
-
-    setState((prevState) => ({
-      ...prevState,
-      loginStatus: "logging-in",
-    }));
-
-    const options: AuthClientLoginOptions = {
-      identityProvider: process.env.II_URL,
-      onSuccess: onLoginSuccess,
-      onError: onLoginError,
-      maxTimeToLive: BigInt(3_600_000_000_000), // Defaults to 1 hour
-      ...loginOptions,
-    };
-
-    state.authClient.login(options);
-  }
-
-  /**
-   * Clears the identity from the state and local storage. Effectively "logs the
-   * user out".
-   */
-  async function clear() {
-    if (!state.authClient) {
-      throw new Error("Auth client not initialized");
-    }
-    await state.authClient.logout();
-
-    setState((prevState) => ({
-      ...prevState,
-      identity: undefined,
-      loginStatus: "idle",
-    }));
-  }
-
-  return (
-    <InternetIdentityContext.Provider
-      value={{
-        isInitializing: state.isInitializing,
-        login,
-        loginStatus: state.loginStatus,
-        loginError: state.loginError,
-        isLoggingIn: state.loginStatus === "logging-in",
-        isLoginError: state.loginStatus === "error",
-        isLoginSuccess: state.loginStatus === "success",
-        isLoginIdle: state.loginStatus === "idle",
-        clear,
-        identity: state.identity,
-      }}
-    >
-      {children}
-    </InternetIdentityContext.Provider>
-  );
+  return children;
 }
-
-export * from "./context.type";
